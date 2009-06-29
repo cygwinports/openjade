ddad702b8a4b99c498c10c911bcbe4d4b92bbf93
yselkowitz <yselkowitz@e127d73d-b18e-4ae1-a744-a22e6a3ccd30>
2009-06-29 05:39:34 +0000

    docbook/openjade: fixed install-catalog cmds
    git-svn-id: https://cygwin-ports.svn.sourceforge.net/svnroot/cygwin-ports/ports/trunk@6903 e127d73d-b18e-4ae1-a744-a22e6a3ccd30

--- postinstall.sh
+++ postinstall.sh
@@ -1,2 +1,2 @@
-install-catalog --add /etc/sgml/catalog /usr/share/sgml/OpenJade/catalog
-install-catalog --add /etc/sgml/catalog /usr/share/sgml/OpenJade/unicode/catalog
+install-catalog --add /etc/sgml/jade.cat /usr/share/sgml/OpenJade/catalog
+install-catalog --add /etc/sgml/jade.cat /usr/share/sgml/OpenJade/unicode/catalog
--- preremove.sh
+++ preremove.sh
@@ -1,2 +1,2 @@
-install-catalog --remove /etc/sgml/catalog /usr/share/sgml/OpenJade/catalog
-install-catalog --remove /etc/sgml/catalog /usr/share/sgml/OpenJade/unicode/catalog
+install-catalog --remove /etc/sgml/jade.cat /usr/share/sgml/OpenJade/catalog
+install-catalog --remove /etc/sgml/jade.cat /usr/share/sgml/OpenJade/unicode/catalog
