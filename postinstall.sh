57e96f4f4a9fb3acae09cfad80ae29877f0adf76
yselkowitz <yselkowitz@e127d73d-b18e-4ae1-a744-a22e6a3ccd30>
2009-06-29 05:28:52 +0000

    docbook/openjade 1.4devel1 (NEW)
    git-svn-id: https://cygwin-ports.svn.sourceforge.net/svnroot/cygwin-ports/ports/trunk@6901 e127d73d-b18e-4ae1-a744-a22e6a3ccd30

---
+++ postinstall.sh
@@ -0,0 +1,2 @@
+install-catalog --add /etc/sgml/catalog /usr/share/sgml/OpenJade/catalog
+install-catalog --add /etc/sgml/catalog /usr/share/sgml/OpenJade/unicode/catalog
---
+++ preremove.sh
@@ -0,0 +1,2 @@
+install-catalog --remove /etc/sgml/catalog /usr/share/sgml/OpenJade/catalog
+install-catalog --remove /etc/sgml/catalog /usr/share/sgml/OpenJade/unicode/catalog
